a64bc7d4eb6bd38def888cd703d6ae456deb5b34
Shubham Agarwal <shubham_ag@outlook.com>
2025-04-24 20:29:28 +0530

    added routes

--- src/main/kotlin/com/bolt/routes/ClassRoute.kt
+++ src/main/kotlin/com/bolt/routes/ClassRoute.kt
@@ -1,2 +1,23 @@
 package com.bolt.routes
 
+
+import com.bolt.service.DnDApiService
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.http.*
+
+fun Route.classRoute() {
+    get("/classes/{className}") {
+        val className = call.parameters["className"] ?: return@get call.respondText(
+            "Missing class name", status = HttpStatusCode.BadRequest
+        )
+
+        val result = DnDApiService.getClassDetails(className)
+        if (result == null) {
+            call.respond(HttpStatusCode.NotFound, "Class not found")
+        } else {
+            call.respond(result)
+        }
+    }
+}
