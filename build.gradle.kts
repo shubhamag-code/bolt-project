3ef5f7649438c7ceb737e1043a2fd907ad9b54c9
Shubham Agarwal <shubham@Shubhams-MacBook-Air.local>
2025-04-24 20:19:15 +0530

    updated build.gradle.kts with ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,33 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.1.0"
+    application
 }
 
-group = "org.example"
+group = "com.bolt"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("com.bolt.MainApplicationKt")
+}
+
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.2")
+    implementation("io.ktor:ktor-server-netty:2.3.2")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.2")
+
+    implementation("io.ktor:ktor-client-core:2.3.2")
+    implementation("io.ktor:ktor-client-cio:2.3.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
